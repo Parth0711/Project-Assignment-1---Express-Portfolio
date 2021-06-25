b3954af5874016f59a451589627409dcf24996ee
Parthkumar Patel <patelparthalarsa@gmail.com>
2021-06-24 23:38:25 -0700

    My first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Parth Patel-Developer' });
+});
+/* GET About Page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+/* GET Contact Page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+/* GET Projects Page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+/* GET Services Page */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Services' });
+});
+module.exports = router;
